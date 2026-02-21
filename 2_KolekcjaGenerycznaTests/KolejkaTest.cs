50c108753230bc6235f8925f562ee6e764e7699f
Bartek <batorskib@gmail.com>
2026-02-21 12:31:58 +0100

    testy kolejki

---
+++ 2_KolekcjaGenerycznaTests/KolejkaTest.cs
@@ -0,0 +1,63 @@
+namespace _2_KolekcjaGenerycznaTests;
+
+[TestClass]
+public class KolejkaTest
+{
+    [TestMethod]
+    public void UzyciePeek()
+    {
+        var kolejka = new Queue<int>();
+        kolejka.Enqueue(1);
+        kolejka.Enqueue(2);
+        kolejka.Enqueue(3);
+        kolejka.Enqueue(4);
+
+        Assert.AreEqual(1, kolejka.Peek());
+    }
+
+    [TestMethod]
+    public void UzycieContains()
+    {
+        var kolejka = new Queue<int>();
+        kolejka.Enqueue(1);
+        kolejka.Enqueue(2);
+        kolejka.Enqueue(3);
+        kolejka.Enqueue(4);
+
+        Assert.IsTrue(kolejka.Contains(3));
+    }
+
+    [TestMethod]
+    public void UzycieToArray()
+    {
+        var kolejka = new Queue<int>();
+        kolejka.Enqueue(1);
+        kolejka.Enqueue(2);
+        kolejka.Enqueue(3);
+        kolejka.Enqueue(4);
+
+        var tablica = kolejka.ToArray();
+        kolejka.Dequeue();
+
+        Assert.AreEqual(1, tablica[0]);
+        Assert.AreEqual(3, kolejka.Count);
+
+    }
+
+    [TestMethod]
+    public void UzycieClear()
+    {
+        var kolejka = new Queue<int>();
+        kolejka.Enqueue(1);
+        kolejka.Enqueue(2);
+        kolejka.Enqueue(3);
+        kolejka.Enqueue(4);
+
+        kolejka.Clear();
+
+        Assert.AreEqual(0, kolejka.Count);
+
+    }
+
+
+}
